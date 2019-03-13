276c225ffb9cdc648f78539fcf9cc777c46eb6a1
Nigam Shah <23nigam@gmail.com>
2019-03-13 23:35:51 +0530

    Added Typed.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,10 @@
 $(function() {
     $(".terminal-output").empty();
+
+    var typed = new Typed("#auto-typing-homepage", {
+	  strings: ["Drop me an email on hello@nigamshah.dev or head over to the shell to make things a bit more exicting"],
+	  typeSpeed : 50
+	});
+
+	$('.typed-cursor').hide();
 });
